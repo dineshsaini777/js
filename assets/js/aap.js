e82d47d2505ce9e713aab93664c5892a05ca91c7
dineshsaini777 <dinesh016622@gmail.com>
2023-08-10 15:00:24 -0400

    changes

--- assets/js/aap.js
+++ assets/js/aap.js
@@ -7,14 +7,18 @@ console.log(".crl")
 let val = 1;
 mybutton.addEventListener("click", function () {
     if (val === 1) {
-        crl.classList.add("on");
+        crl.classList.add("img");
+        crl.classList.remove("on");
         mybutton.classList.remove("font");
+        mybutton.classList.remove("butn2")
         mybutton.innerHTML = "off";
         val = 0
     }
     else if (val === 0) {
-        crl.classList.remove("on");
+        crl.classList.remove("img");
+        crl.classList.add("on");
         mybutton.classList.add("font");
+        mybutton.classList.add("butn2")
         mybutton.innerHTML = "on";
         val = 1
     }
